d0a27e8db591e02097327ee57cd39544f0c64472
Terry <shiyuntian@126.com>
2023-04-18 22:32:41 -0400

    change site title

--- components/layout.js
+++ components/layout.js
@@ -5,7 +5,7 @@ import utilStyles from "../styles/utils.module.css";
 import Link from "next/link";
 
 const name = "Terry Shi";
-export const siteTitle = "Next.js Sample Website";
+export const siteTitle = "Terry's Website";
 
 export default function Layout({ children, home }) {
   return (
